41bbbd6de7cbe8e3bdc56a58ad9fe803a66da552
JOTHIR ADITHYA <tkjoadi2004@gmail.com>
2025-10-01 18:44:49 +0530

    Add files via upload

---
+++ Exercise2/Exercise2/Smart_Home_System/Observer/SmartHub.cs
@@ -0,0 +1,33 @@
+﻿using Smart_Home_System.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Home_System.Observer
+{
+    public class SmartHub
+    {
+        private static SmartHub instance;
+        private List<ISmartDevice> devices = new List<ISmartDevice>();
+
+        private SmartHub() { }
+
+        public static SmartHub Instance => instance ??= new SmartHub();
+
+        public IReadOnlyList<ISmartDevice> Devices => devices.AsReadOnly();
+
+        public void RegisterDevice(ISmartDevice device)
+        {
+            devices.Add(device);
+            Console.WriteLine($"Device registered: {device.Name} {device.Id}");
+        }
+
+        public void ShowStatus()
+        {
+            foreach (var d in devices)
+                Console.WriteLine(d.GetStatus());
+        }
+    }
+}
